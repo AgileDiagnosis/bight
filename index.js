443c5684ddfb2902a9a18f5116709def683c08d0
jden <jason@denizac.org>
2013-02-18 14:42:50 -0800

    fix implementation

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var Stream = require('stream')
 
 function bight(streams) {
+
   if (!Array.isArray(streams)) {
-    streams = Array.prototype.slice.call(streams)
+    streams = Array.prototype.slice.call(arguments)
   }
+
   if (!(this instanceof bight)) {
     return new bight(streams)
   }
@@ -12,15 +14,29 @@ function bight(streams) {
   inStream.writable = true
   inStream.readable = true
   inStream.write = function (data) {
-    inStream.emit('data', data)
+    innerStream.emit('data', data)
   }
   inStream.end = function (data) {
+    inStream.writable = false
+    innerStream.emit('end', typeof data === 'undefined' ? null : data)
+  }
+
+  var innerStream = new Stream()
+  innerStream.writable = true
+  innerStream.readable = true
+  innerStream.write = function (data) {
+    inStream.emit('data', data)
+  }
+  innerStream.end = function (data) {
+    innerStream.writable = false
     inStream.emit('end', typeof data === 'undefined' ? null : data)
   }
 
-  return streams.reduce(function (prev, next) {
+  var end = streams.reduce(function (prev, next) {
     return prev.pipe(next)
-  }, inStream)
+  }, innerStream).pipe(innerStream)
+
+  return inStream
 }
 
 module.exports = bight
--- test/bight.js
+++ test/bight.js
@@ -8,36 +8,29 @@ describe('bight', function () {
   it('composes through streams and returns a composite through stream', function (done) {
 
     var split = require('split')
-    var toUpper = require('toupper')
-    var exclaim = require('through')(function (d) { this.queue(d + '!') })
+    var through = require('through')
+    var toUpper = through(function (d) { this.queue(d.toString().toUpperCase()) })
+    var exclaim = through(function (d) { this.queue(d + '!') })
 
-    var superMode = bight(split(), toUpper())
 
-
-    var input = 'rar\npunch'
     var source = new Stream()
-    var next = function () {
-      process.nextTick(function () {
-        if (!input) {
-          return source.emit('end', null)
-        }
-        var head = input[0]
-        var input = input.substr(1)
-        source.emit('data', head)
-        next()
-      })
-    }
 
-    var test = source.pipe(superMode)
+    var test = source.pipe(bight(split(), toUpper, exclaim))
 
     var expected = ['RAR!','PUNCH!']
+    var chunks = 0
     test.on('data', function (data) {
       data.should.equal(expected.shift())
+      chunks++
     })
-    test.on('end', done)
+    test.on('end', function () {
+      chunks.should.equal(2)
+      done()
+    })
     test.on('error', done)
 
-    next()
+    source.emit('data', 'rar\npunch')
+    source.emit('end')
 
   })
 })
